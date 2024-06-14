b29cc2083ac363652d853de0b3b5652390afd1eb
AdityaShinde716 <164852169+AdityaShinde716@users.noreply.github.com>
2024-06-14 12:25:17 +0530

    Update Ecommerce.jsx

--- src/pages/Ecommerce.jsx
+++ src/pages/Ecommerce.jsx
@@ -303,8 +303,7 @@ const Ecommerce = () => {
               <p className="font-semibold text-lg">React 19 coming soon!</p>
               <p className="text-gray-400 ">By Aditya Shinde</p>
               <p className="mt-8 text-sm text-gray-400">
-                This will be the small description for the news you have shown
-                here. There could be some great info.
+                This is where the info about React 19 will be shown. 
               </p>
               <div className="mt-3">
                 <Button
